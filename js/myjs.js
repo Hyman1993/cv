d11719a34d79ce82c33f9e5b494b3560524a39fb
彭煌 <houkua@pm.nttdata.co.jp>
2019-05-28 16:07:32 +0900

    2019.05.28 延迟加载效果修正

--- js/myjs.js
+++ js/myjs.js
@@ -35,20 +35,11 @@ $(function(){  //文档加载完毕时调用
 		 lastScrollTop = scroH;
     });
 
- // 图片延迟加载
- $(".cross-image img").delayLoading({
-                defaultImg: "img/loading.gif",   // 预加载前显示的图片  
-                errorImg: "",   // 读取图片错误时替换图片(默认：与defaultImg一样)  
-                imgSrcAttr: "originalSrc",//记录图片路径的属性(默认：originalSrc，页面img的src属性也要替换为originalSrc)  
-                beforehand: 0,  // 预先提前多少像素加载图片(默认：0)  
-                event: "scroll", // 触发加载图片事件(默认：scroll)  
-                duration: "slow", // 三种预定淡出(入)速度之一的字符串("slow", "normal", or "fast")或表示动画时长的毫秒数值(如：1000),默认:"normal"  
-                container: window,     // 对象加载的位置容器(默认：window)  
-                success: function (imgObj) { console.log("图片加载成功")}, // 加载图片成功后的回调函数(默认：不执行任何操作)  
-                error: function (imgObj) {console.log("图片加载失败") }  // 加载图片失败后的回调函数(默认：不执行任何操作)  
-            });
+      // 图片延迟加载 class="lazyload" data-src="img/mountains/mountain_1.jpg" src="img/loading.gif"
+      lazyload();
 
 });  
+
 
    // 导航栏快速定位
 function goTo(idName,obj){
